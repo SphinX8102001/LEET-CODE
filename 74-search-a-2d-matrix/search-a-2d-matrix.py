3e7b0354c77d60707921aa21fba517980ca690df
Al-Washi Sarker <alwashisarker@gmail.com>
2024-03-15 04:53:41 +0600

    Time: 20 ms (85.10%) | Memory: 12.2 MB (11.99%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        row = len(matrix)
+        for i in range(row):
+            if target in matrix[i]:
+                return True
+        return False
